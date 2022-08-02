8e79836273009621d3f5ff669830b8302ce73232
João Paulo de Fáveri <joao.faveri@gmail.com>
2022-08-02 14:26:43 +0000

    ✨ feat(auth): implemnet passport local

--- src/auth/local.strategy.ts
+++ src/auth/local.strategy.ts
@@ -1,4 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy } from 'passport-local';
+import { ValidateUserDto } from './dto/validate-user.dto';
+import { User } from '@prisma/client';
 
 @Injectable()
-export class LocalStrategy {}
+export class LocalStrategy extends PassportStrategy(Strategy) {
+  constructor(private authService: AuthService) {
+    super();
+  }
+
+  async validate(login: ValidateUserDto): Promise<User | Error> {
+    const user = await this.authService.validateUser(login);
+
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+
+    return user;
+  }
+}
